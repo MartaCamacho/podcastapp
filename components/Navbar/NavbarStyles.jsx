4a1b94d2e94c239021c262e8f327b11f5784c2c5
martacamacho <martacamachovargas@gmail.com>
2023-03-16 21:38:54 +0100

    created and styled navbar

---
+++ components/Navbar/NavbarStyles.jsx
@@ -0,0 +1,14 @@
+import css from 'styled-jsx/css';
+
+export default css`
+.navbar-container {
+    font-weight: bold;
+    color: #546dc3; 
+    height: 60px;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    border-bottom: 1px solid gray;
+    font-size: 20px;
+}
+`
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Link from 'next/link';
+import styles from './NavbarStyles';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar-container">
+    <Link href="/">
+      Podcaster
+    </Link>
+      <style jsx>{styles}</style>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
